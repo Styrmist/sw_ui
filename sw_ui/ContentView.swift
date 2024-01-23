f7b817f41e9c19d9d2119c657c87def70d9c6526
Kyrylo Danylov <laziest@tuta.io>
2024-01-23 22:16:35 +0200

    idea 1

--- sw_ui/ContentView.swift
+++ sw_ui/ContentView.swift
@@ -14,29 +14,68 @@ struct ContentView: View {
             Color.gray
                 .ignoresSafeArea()
             
-//            GeometryReader { all in
+            HStack(spacing: .zero) {
+                Spacer(minLength: 50)
                 
-                HStack(spacing: .zero) {
-                    Spacer(minLength: 50)
+                VStack(spacing: .zero) {
+                    Spacer(minLength: 20)
                     
-                    VStack(spacing: .zero) {
-                        Spacer(minLength: 20)
-                        verticalUpper
-                        Spacer(minLength: 20)
-                        imageView
-                            .layoutPriority(1)
-                        Spacer()
-                            .frame(height: 15)
-                        Text("nanit")
-                        Spacer()
-                            .layoutPriority(1)
+                    
+                    Text("Today Chirstiano Ronaldo is")
+                    Spacer()
+                        .frame(height: 13)
+                    HStack(spacing: 22) {
+                        Rectangle()
+                            .foregroundStyle(.blue)
+                        Rectangle()
+                            .foregroundStyle(.red)
+                        Rectangle()
+                            .foregroundStyle(.blue)
                     }
+                    .frame(height: 88)
+                    Spacer()
+                        .frame(height: 14)
+                    Text("Years old")
                     
-                    Spacer(minLength: 50)
+                    
+                    Spacer(minLength: 20)
+                    
+                    GeometryReader { proxy in
+                        let width = proxy.size.width
+                        let height = proxy.size.height
+                        
+                        let smallCircleX = width / 2 + cos(degreesToRadians(angleOffset)) * (width / 2 - smallSize / 2)
+                        let smallCircleY = height / 2 - sin(degreesToRadians(angleOffset)) * (height / 2 - smallSize / 2)
+                        
+                        ZStack {
+                            Circle()
+                                .frame(width: width, height: height)
+                                .foregroundColor(Color.yellow)
+                                .overlay {
+                                    backgroundView
+                                }
+                            
+                            Circle()
+                                .frame(width: smallSize, height: smallSize)
+                                .position(x: smallCircleX, y: smallCircleY)
+                                .foregroundColor(Color.red)
+                                .offset(x: -smallSize / 2, y: -smallSize / 1.5)
+                        }
+                    }
+                    .layoutPriority(1)
+                    .zIndex(-1)
+                    
+                    
+                    Spacer()
+                        .frame(height: 15)
+                    Text("nanit")
+                    Spacer()
+                        .layoutPriority(1)
                 }
-//            }
+                
+                Spacer(minLength: 50)
+            }
         }
-        
     }
     
     @ViewBuilder
@@ -47,50 +86,8 @@ struct ContentView: View {
             .ignoresSafeArea()
     }
     
-    var verticalUpper: some View {
-        VStack() {
-            Text("Today Chirstiano Ronaldo is")
-            Spacer()
-                    .frame(height: 13)
-            HStack(spacing: 22) {
-                Rectangle()
-                    .foregroundStyle(.blue)
-                Rectangle()
-                    .foregroundStyle(.red)
-                Rectangle()
-                    .foregroundStyle(.blue)
-            }
-            .frame(height: 88)
-            Spacer()
-                    .frame(height: 14)
-            Text("Years old")
-        }
-    }
-    
     let smallSize: CGFloat = 36
     let angleOffset: Double = 45
-    
-    var imageView: some View {
-            GeometryReader { proxy in
-                let width = proxy.size.width
-                let height = proxy.size.height
-                
-                let smallCircleX = width / 2 + cos(degreesToRadians(angleOffset)) * (width / 2 - smallSize / 2)
-                let smallCircleY = height / 2 - sin(degreesToRadians(angleOffset)) * (height / 2 - smallSize / 2)
-                
-                ZStack {
-                    Circle()
-                        .frame(width: width, height: height)
-                        .foregroundColor(Color.yellow)
-                    
-                    Circle()
-                        .frame(width: smallSize, height: smallSize)
-                        .position(x: smallCircleX, y: smallCircleY)
-                        .foregroundColor(Color.red)
-                        .offset(x: -smallSize / 2, y: -smallSize / 1.5)
-                }
-            }
-    }
     
 //    func imageView(_ gproxy: GeometryProxy) -> some View {
 //        let size = gproxy.size
