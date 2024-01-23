6f7795f0489c6174e9bf5a6633024a4d5973714b
Kyrylo Danylov <laziest@tuta.io>
2024-01-23 22:45:08 +0200

    working prototype

--- sw_ui/ContentView.swift
+++ sw_ui/ContentView.swift
@@ -9,117 +9,99 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let smallSize: CGFloat = 36
+    let angleOffset: Double = 45
+    
     var body: some View {
         ZStack {
             Color.gray
                 .ignoresSafeArea()
             
-            HStack(spacing: .zero) {
-                Spacer(minLength: 50)
+            GeometryReader { all in
                 
-                VStack(spacing: .zero) {
-                    Spacer(minLength: 20)
+                HStack(spacing: .zero) {
+                    Spacer(minLength: 50)
                     
-                    
-                    Text("Today Chirstiano Ronaldo is")
-                    Spacer()
-                        .frame(height: 13)
-                    HStack(spacing: 22) {
-                        Rectangle()
-                            .foregroundStyle(.blue)
-                        Rectangle()
-                            .foregroundStyle(.red)
-                        Rectangle()
-                            .foregroundStyle(.blue)
-                    }
-                    .frame(height: 88)
-                    Spacer()
-                        .frame(height: 14)
-                    Text("Years old")
-                    
-                    
-                    Spacer(minLength: 20)
-                    
-                    GeometryReader { proxy in
-                        let width = proxy.size.width
-                        let height = proxy.size.height
+                    VStack(spacing: .zero) {
+                        Spacer(minLength: 20)
                         
-                        let smallCircleX = width / 2 + cos(degreesToRadians(angleOffset)) * (width / 2 - smallSize / 2)
-                        let smallCircleY = height / 2 - sin(degreesToRadians(angleOffset)) * (height / 2 - smallSize / 2)
+                        topView
                         
-                        ZStack {
-                            Circle()
-                                .frame(width: width, height: height)
-                                .foregroundColor(Color.yellow)
-                                .overlay {
-                                    backgroundView
-                                }
-                            
-                            Circle()
-                                .frame(width: smallSize, height: smallSize)
-                                .position(x: smallCircleX, y: smallCircleY)
-                                .foregroundColor(Color.red)
-                                .offset(x: -smallSize / 2, y: -smallSize / 1.5)
-                        }
-                    }
-                    .layoutPriority(1)
-                    .zIndex(-1)
-                    
-                    
-                    Spacer()
-                        .frame(height: 15)
-                    Text("nanit")
-                    Spacer()
+                        Spacer(minLength: 20)
+                        
+                        avatar(all.size)
                         .layoutPriority(1)
+                        .zIndex(-1)
+                        
+                        Spacer()
+                            .frame(height: 15)
+                        
+                        Text("nanit")
+                        
+                        Spacer()
+                            .layoutPriority(1)
+                    }
+                    
+                    Spacer(minLength: 50)
                 }
-                
-                Spacer(minLength: 50)
             }
         }
     }
     
-    @ViewBuilder
-    var backgroundView: some View {
+    func backgroundView(_ size: CGSize) -> some View {
         Image(.background)
             .resizable()
             .aspectRatio(contentMode: .fill)
             .ignoresSafeArea()
+            .frame(width: size.width, height: size.height)
+            .offset(y: 80)
     }
     
-    let smallSize: CGFloat = 36
-    let angleOffset: Double = 45
+    @ViewBuilder
+    var topView: some View {
+        Text("Today Chirstiano Ronaldo is")
+        Spacer()
+            .frame(height: 13)
+        HStack(spacing: 22) {
+            Rectangle()
+                .foregroundStyle(.blue)
+            Rectangle()
+                .foregroundStyle(.red)
+            Rectangle()
+                .foregroundStyle(.blue)
+        }
+        .frame(height: 88)
+        Spacer()
+            .frame(height: 14)
+        Text("Years old")
+    }
     
-//    func imageView(_ gproxy: GeometryProxy) -> some View {
-//        let size = gproxy.size
-//            
-//        return
-//        ZStack {
-//
-//            GeometryReader { proxy in
-//                let width = proxy.size.width
-//                let height = proxy.size.height
-//                
-//                let smallCircleX = width / 2 + cos(degreesToRadians(angleOffset)) * (width / 2 - smallSize / 2)
-//                let smallCircleY = height / 2 - sin(degreesToRadians(angleOffset)) * (height / 2 - smallSize / 2)
-//                
-//                ZStack {
-//                    Circle()
-//                        .frame(width: width, height: height)
-//                        .foregroundColor(Color.yellow)
-//                    
-//                    Circle()
-//                        .frame(width: smallSize, height: smallSize)
-//                        .position(x: smallCircleX, y: smallCircleY)
-//                        .foregroundColor(Color.red)
-//                        .offset(x: -smallSize / 2, y: -smallSize / 2)
-//                }
-//            }
-//            
-////            backgroundView
-////                .frame(width: size.width, height: size.height)
-////                .offset(x: -50, y: -size.width / 2)
-//        }
-//    }
+    @ViewBuilder
+    func avatar(_ allSize: CGSize) -> some View {
+        GeometryReader { proxy in
+            let width: CGFloat = proxy.size.width
+            let height: CGFloat = proxy.size.height
+            
+            let smallCircleX: CGFloat = width / 2 + cos(degreesToRadians(angleOffset)) * (width / 2 - smallSize / 2)
+            let smallCircleY: CGFloat = height / 2 - sin(degreesToRadians(angleOffset)) * (height / 2 - smallSize / 2)
+            
+            ZStack {
+                Circle()
+                    .frame(width: width, height: height)
+                    .foregroundColor(Color.yellow)
+                    .overlay {
+                        backgroundView(allSize)
+                        
+                    }
+                
+                Circle()
+                    .frame(width: smallSize, height: smallSize)
+                    .position(x: smallCircleX, y: smallCircleY)
+                    .foregroundColor(Color.red)
+                    .offset(x: -smallSize / 2, y: -smallSize / 1.5)
+            }
+        }
+    }
     
     func degreesToRadians(_ degrees: Double) -> CGFloat {
             return CGFloat(degrees * .pi / 180)
