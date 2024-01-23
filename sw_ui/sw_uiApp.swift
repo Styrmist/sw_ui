ab75514de38e6aa0ebe8de9991376cf7eecac3fc
Kyrylo Danylov <laziest@tuta.io>
2024-01-23 20:01:05 +0200

    Initial Commit

---
+++ sw_ui/sw_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sw_uiApp.swift
+//  sw_ui
+//
+//  Created by Kirill Danilov on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct sw_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
